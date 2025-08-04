51bd6eaeb15723bcb265e70343c3d05baa3090fb
Yiming Cao <yiming@whimslab.io>
2025-08-04 20:10:14 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,44 +1,9 @@
-import { SqlJsDialect } from '@zenstackhq/kysely-sql-js';
-import { ZenStackClient } from '@zenstackhq/runtime';
 import { inspect } from 'node:util';
-import initSqlJs from 'sql.js';
-import { schema } from './zenstack/schema';
+import { createClient } from './db';
 
 async function main() {
-  // initialize sql.js engine
-  const SQL = await initSqlJs();
+  const db = await createClient();
 
-  // create database client with sql.js dialect
-  const db = new ZenStackClient(schema, {
-    dialect: new SqlJsDialect({ sqlJs: new SQL.Database() }),
-  });
-
-  // push schema to the database (`$pushSchema` is for testing only)
-  await db.$pushSchema();
-
-  // create a user with some postsØ
-  const user = await db.user.create({
-    data: {
-      email: 'u1@test.com',
-      posts: {
-        create: [
-          {
-            title: 'Post1',
-            content: 'My first post',
-            published: false,
-          },
-          {
-            title: 'Post2',
-            content: 'Just another post',
-            published: true,
-          },
-        ],
-      },
-    },
-    include: { posts: true },
-  });
-
-  console.log(inspect(user, { colors: true }));
 }
 
 main();
